cd315004abb1c6e613852537e455aca36930c183
csekhar193 <chandrasekhar193.ece@gmail.com>
2018-08-05 14:50:43 +0530

    modified the mongodb uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://csekhar193:w433iqoq@ds137281.mlab.com:37281/todoapp' || 'mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
 
 module.exports = {mongoose};
